68a68c751c95bb40dfc087e074731668856ff3d2
Samy Pessé <samypesse@gmail.com>
2014-07-26 20:04:05 -0700

    Add more methods to rpc service

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 define(function(Manager) {
+    var rpc = codebox.require("core/rpc");
     var commands = codebox.require("core/commands");
 
+    var cmdBranchSwitch, cmdInit;
 
-    commands.register({
+    // Toggle commands that need git to be ok
+    var toggleStatus = function(state) {
+        _.invoke([
+            cmdBranchSwitch
+        ], "set", "hidden", !state);
+        _.invoke([
+            cmdInit
+        ], "set", "hidden", state);
+    };
+
+    // Check git status
+    var updateStatus = function(p) {
+        return (p || rpc.execute("git/status"))
+        .then(function() {
+            toggleStatus(true);
+        }, function(err) {
+            toggleStatus(false);
+        })
+    };
+
+    ///// branches
+
+    cmdBranchSwitch = commands.register({
         id: "git.branch.change",
-        title: "Git: Switch to Branch",
+        title: "Git: Switch To Branch",
         run: function(args, context) {
 
         }
     });
+
+    ///// Init/Clone
+
+    cmdInit = commands.register({
+        id: "git.init",
+        title: "Git: Init",
+        run: function(args, context) {
+            return updateStatus(rpc.execute("git/init"));
+        }
+    });
+
+
+    updateStatus();
 });
