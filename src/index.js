323ba2624366fb069b35747f2a2a01709f90747d
Samy Pessé <samypesse@gmail.com>
2015-04-08 16:45:17 +0200

    Adapt to browserify

--- src/index.js
+++ src/index.js
@@ -1,299 +1,297 @@
-define([
-    "src/settings",
-    "src/menu",
-    "text!src/templates/branch.html",
-    "text!src/templates/file.html"
-], function(settings, initMenu, templateBranch, templateFile) {
-    var Q = codebox.require("hr/promise");
-    var rpc = codebox.require("core/rpc");
-    var commands = codebox.require("core/commands");
-    var dialogs = codebox.require("utils/dialogs");
+var settings = require("./settings");
+var initMenu = require("./menu");
+var templateBranch = require("./templates/branch.html");
+var templateFile = require("./templates/file.html");
 
-    var cmdBranchSwitch, cmdBranchCreate, cmdBranchDelete, cmdInit, cmdClone, cmdCommit, cmdPush, cmdSync, cmdStatus;
+var Q = codebox.require("q");
+var rpc = codebox.require("core/rpc");
+var commands = codebox.require("core/commands");
+var dialogs = codebox.require("utils/dialogs");
 
-    // Toggle commands that need git to be ok
-    var toggleStatus = function(state) {
-        _.invoke([
-            cmdBranchSwitch, cmdBranchCreate, cmdCommit, cmdPush, cmdPull, cmdSync,
-            cmdStatus, cmdBranchDelete
-        ], "set", "hidden", !state);
+var cmdBranchSwitch, cmdBranchCreate, cmdBranchDelete, cmdInit, cmdClone, cmdCommit, cmdPush, cmdSync, cmdStatus;
 
-        _.invoke([
-            cmdInit, cmdClone
-        ], "set", "hidden", state);
-    };
+// Toggle commands that need git to be ok
+var toggleStatus = function(state) {
+    _.invoke([
+        cmdBranchSwitch, cmdBranchCreate, cmdCommit, cmdPush, cmdPull, cmdSync,
+        cmdStatus, cmdBranchDelete
+    ], "set", "hidden", !state);
 
-    // Check git status
-    var updateStatus = function(p) {
-        return (p || rpc.execute("git/status"))
-        .then(function(d) {
-            toggleStatus(true);
-            return d;
-        }, function(err) {
-            toggleStatus(false);
-            return Q.reject(err);
-        })
-    };
+    _.invoke([
+        cmdInit, cmdClone
+    ], "set", "hidden", state);
+};
 
-    // Handle http auth
-    var handleHttpAuth = function(method) {
-        return Q(method())
-        .fail(function(err) {
-            if (err.code == 401) {
-                // Fields for https auth
-                var fields = {
-                    username: {
+// Check git status
+var updateStatus = function(p) {
+    return (p || rpc.execute("git/status"))
+    .then(function(d) {
+        toggleStatus(true);
+        return d;
+    }, function(err) {
+        toggleStatus(false);
+        return Q.reject(err);
+    })
+};
+
+// Handle http auth
+var handleHttpAuth = function(method) {
+    return Q(method())
+    .fail(function(err) {
+        if (err.code == 401) {
+            // Fields for https auth
+            var fields = {
+                username: {
+                    type: "string",
+                    decription: "Username"
+                },
+                password: {
+                    type: "string",
+                    description: "Password",
+                    password: true
+                }
+            };
+
+            // Passphrase for ssh
+            if (err.message.toLowerCase().indexOf("authentication") < 0) {
+                fields = {
+                    passphrase: {
                         type: "string",
-                        decription: "Username"
-                    },
-                    password: {
-                        type: "string",
-                        description: "Password",
-                        password: true
+                        label: "Passphrase"
                     }
                 };
-
-                // Passphrase for ssh
-                if (err.message.toLowerCase().indexOf("authentication") < 0) {
-                    fields = {
-                        passphrase: {
-                            type: "string",
-                            label: "Passphrase"
-                        }
-                    };
-                }
-
-                return dialogs.schema({
-                    title: "Need authentication:",
-                    properties: fields
-                })
-                .then(method);
-            } else {
-                return Q.reject(err);
             }
-        })
-    };
 
-    ///// Branches
-
-    var selectBranch = function() {
-        return codebox.statusbar.loading(
-            rpc.execute("git/branches"),
-            {
-                prefix: "Listing branches"
-            }
-        )
-        .fail(dialogs.error)
-        .then(function(branches) {
-            return dialogs.list(branches, {
-                template: templateBranch
-            })
-        });
-    };
-
-    cmdBranchSwitch = commands.register({
-        id: "git.branch.change",
-        title: "Git: Switch To Branch",
-        run: function(args, context) {
-            return selectBranch()
-            .then(function(branch) {
-                return codebox.statusbar.loading(
-                    rpc.execute("git/checkout", {
-                        'ref': branch.get("name")
-                    }),
-                    {
-                        prefix: "Checkout '"+branch.get("name")+"'"
-                    }
-                ).fail(dialogs.error);
-            });
-        }
-    });
-
-    cmdBranchCreate = commands.register({
-        id: "git.branch.create",
-        title: "Git: Create New Branch",
-        run: function(args, context) {
-            return dialogs.prompt("Create a branch")
-            .then(function(branch) {
-                return codebox.statusbar.loading(
-                    rpc.execute("git/branch_create", {
-                        'name': branch
-                    }),
-                    {
-                        prefix: "Creating branch '"+branch+"'"
-                    }
-                ).fail(dialogs.error);
-            });
-        }
-    });
-
-    cmdBranchDelete = commands.register({
-        id: "git.branch.delete",
-        title: "Git: Delete a Branch",
-        run: function(args, context) {
-            return selectBranch()
-            .then(function(branch) {
-                return codebox.statusbar.loading(
-                    rpc.execute("git/branch_delete", {
-                        'name': branch.get("name")
-                    }),
-                    {
-                        prefix: "Removing '"+branch.get("name")+"'"
-                    }
-                ).fail(dialogs.error);
-            });
-        }
-    });
-
-    ///// Init/Clone
-
-    cmdInit = commands.register({
-        id: "git.init",
-        title: "Git: Init",
-        run: function(args, context) {
-            return updateStatus(rpc.execute("git/init"));
-        }
-    });
-
-    cmdClone = commands.register({
-        id: "git.clone",
-        title: "Git: Clone Remote",
-        run: function(args, context) {
-            return dialogs.prompt("Clone repository:")
-            .then(function(url) {
-                return codebox.statusbar.loading(
-                    handleHttpAuth(function(creds) {
-                        return updateStatus(rpc.execute("git/clone", {
-                            'url': url,
-                            'auth': creds || {}
-                        }));
-                    }),
-                    {
-                        prefix: "Cloning repository"
-                    }
-                ).fail(dialogs.error);
-            });
-        }
-    });
-
-    ///// Commit
-
-    cmdCommit = commands.register({
-        id: "git.commit",
-        title: "Git: Commit Changes",
-        run: function(args, context) {
             return dialogs.schema({
-                title: "Commit Changes:",
-                properties: {
-                    message: {
-                        description: "Message:",
-                        type: "string"
-                    },
-                    files: {
-                        description: "Files (paths separeated by coma, * for all changes)",
-                        type: "string",
-                        default: "*"
-                    }
-                }
-            }, args)
-            .then(function(infos) {
-                infos.name = settings.data.get("name");
-                infos.email = settings.data.get("email");
-
-                if (infos.files == "*") {
-                    infos.files = [];
-                } else {
-                    infos.files = infos.files.split(",");
-                }
-
-
-                return codebox.statusbar.loading(
-                    rpc.execute("git/commit", infos),
-                    {
-                        prefix: "Commiting"
-                    }
-                ).fail(dialogs.error);
-            });
+                title: "Need authentication:",
+                properties: fields
+            })
+            .then(method);
+        } else {
+            return Q.reject(err);
         }
-    });
+    })
+};
 
-    ///// Status
+///// Branches
 
-    cmdStatus = commands.register({
-        id: "git.status",
-        title: "Git: Status",
-        run: function() {
-            return updateStatus()
-            .fail(dialogs.error)
-            .then(function(status) {
-                var files = _.map(status.files, function(st, fileName) {
-                    return {
-                        'status': st,
-                        'filename': fileName
-                    }
-                });
-
-                if (files.length == 0) return dialogs.alert("nothing to commit, working directory clean");
-
-                return dialogs.list(files, {
-                    template: templateFile
-                })
-                .then(function(file) {
-                    return cmdCommit.run({
-                        files: file.get("filename")
-                    });
-                });
-            });
+var selectBranch = function() {
+    return codebox.statusbar.loading(
+        rpc.execute("git/branches"),
+        {
+            prefix: "Listing branches"
         }
+    )
+    .fail(dialogs.error)
+    .then(function(branches) {
+        return dialogs.list(branches, {
+            template: templateBranch
+        })
     });
+};
 
-    ///// Remote sync
-
-    cmdPush = commands.register({
-        id: "git.push",
-        title: "Git: Push",
-        run: function() {
+cmdBranchSwitch = commands.register({
+    id: "git.branch.change",
+    title: "Git: Switch To Branch",
+    run: function(args, context) {
+        return selectBranch()
+        .then(function(branch) {
             return codebox.statusbar.loading(
-                handleHttpAuth(function(creds) {
-                    return rpc.execute("git/push");
+                rpc.execute("git/checkout", {
+                    'ref': branch.get("name")
                 }),
                 {
-                    prefix: "Pushing"
+                    prefix: "Checkout '"+branch.get("name")+"'"
                 }
             ).fail(dialogs.error);
-        }
-    });
-
-    cmdPull = commands.register({
-        id: "git.pull",
-        title: "Git: Pull",
-        run: function() {
-            return codebox.statusbar.loading(
-                handleHttpAuth(function(creds) {
-                    return rpc.execute("git/pull");
-                }),
-                {
-                    prefix: "Pulling"
-                }
-            ).fail(dialogs.error);
-        }
-    });
-
-    cmdSync = commands.register({
-        id: "git.sync",
-        title: "Git: Sync",
-        run: function() {
-            return codebox.statusbar.loading(
-                handleHttpAuth(function(creds) {
-                    return rpc.execute("git/sync");
-                }),
-                {
-                    prefix: "Syncing (pull & push)"
-                }
-            ).fail(dialogs.error);
-        }
-    });
-
-    updateStatus();
-    initMenu();
+        });
+    }
 });
+
+cmdBranchCreate = commands.register({
+    id: "git.branch.create",
+    title: "Git: Create New Branch",
+    run: function(args, context) {
+        return dialogs.prompt("Create a branch")
+        .then(function(branch) {
+            return codebox.statusbar.loading(
+                rpc.execute("git/branch_create", {
+                    'name': branch
+                }),
+                {
+                    prefix: "Creating branch '"+branch+"'"
+                }
+            ).fail(dialogs.error);
+        });
+    }
+});
+
+cmdBranchDelete = commands.register({
+    id: "git.branch.delete",
+    title: "Git: Delete a Branch",
+    run: function(args, context) {
+        return selectBranch()
+        .then(function(branch) {
+            return codebox.statusbar.loading(
+                rpc.execute("git/branch_delete", {
+                    'name': branch.get("name")
+                }),
+                {
+                    prefix: "Removing '"+branch.get("name")+"'"
+                }
+            ).fail(dialogs.error);
+        });
+    }
+});
+
+///// Init/Clone
+
+cmdInit = commands.register({
+    id: "git.init",
+    title: "Git: Init",
+    run: function(args, context) {
+        return updateStatus(rpc.execute("git/init"));
+    }
+});
+
+cmdClone = commands.register({
+    id: "git.clone",
+    title: "Git: Clone Remote",
+    run: function(args, context) {
+        return dialogs.prompt("Clone repository:")
+        .then(function(url) {
+            return codebox.statusbar.loading(
+                handleHttpAuth(function(creds) {
+                    return updateStatus(rpc.execute("git/clone", {
+                        'url': url,
+                        'auth': creds || {}
+                    }));
+                }),
+                {
+                    prefix: "Cloning repository"
+                }
+            ).fail(dialogs.error);
+        });
+    }
+});
+
+///// Commit
+
+cmdCommit = commands.register({
+    id: "git.commit",
+    title: "Git: Commit Changes",
+    run: function(args, context) {
+        return dialogs.schema({
+            title: "Commit Changes:",
+            properties: {
+                message: {
+                    description: "Message:",
+                    type: "string"
+                },
+                files: {
+                    description: "Files (paths separeated by coma, * for all changes)",
+                    type: "string",
+                    default: "*"
+                }
+            }
+        }, args)
+        .then(function(infos) {
+            infos.name = settings.data.get("name");
+            infos.email = settings.data.get("email");
+
+            if (infos.files == "*") {
+                infos.files = [];
+            } else {
+                infos.files = infos.files.split(",");
+            }
+
+
+            return codebox.statusbar.loading(
+                rpc.execute("git/commit", infos),
+                {
+                    prefix: "Commiting"
+                }
+            ).fail(dialogs.error);
+        });
+    }
+});
+
+///// Status
+
+cmdStatus = commands.register({
+    id: "git.status",
+    title: "Git: Status",
+    run: function() {
+        return updateStatus()
+        .fail(dialogs.error)
+        .then(function(status) {
+            var files = _.map(status.files, function(st, fileName) {
+                return {
+                    'status': st,
+                    'filename': fileName
+                }
+            });
+
+            if (files.length == 0) return dialogs.alert("nothing to commit, working directory clean");
+
+            return dialogs.list(files, {
+                template: templateFile
+            })
+            .then(function(file) {
+                return cmdCommit.run({
+                    files: file.get("filename")
+                });
+            });
+        });
+    }
+});
+
+///// Remote sync
+
+cmdPush = commands.register({
+    id: "git.push",
+    title: "Git: Push",
+    run: function() {
+        return codebox.statusbar.loading(
+            handleHttpAuth(function(creds) {
+                return rpc.execute("git/push");
+            }),
+            {
+                prefix: "Pushing"
+            }
+        ).fail(dialogs.error);
+    }
+});
+
+cmdPull = commands.register({
+    id: "git.pull",
+    title: "Git: Pull",
+    run: function() {
+        return codebox.statusbar.loading(
+            handleHttpAuth(function(creds) {
+                return rpc.execute("git/pull");
+            }),
+            {
+                prefix: "Pulling"
+            }
+        ).fail(dialogs.error);
+    }
+});
+
+cmdSync = commands.register({
+    id: "git.sync",
+    title: "Git: Sync",
+    run: function() {
+        return codebox.statusbar.loading(
+            handleHttpAuth(function(creds) {
+                return rpc.execute("git/sync");
+            }),
+            {
+                prefix: "Syncing (pull & push)"
+            }
+        ).fail(dialogs.error);
+    }
+});
+
+updateStatus();
+initMenu();
--- src/settings.js
+++ src/settings.js
@@ -1,20 +1,18 @@
-define(function() {
-    return codebox.settings.schema("git",
-        {
-            "title": "Git",
-            "type": "object",
-            "properties": {
-                "email": {
-                    "description": "Email",
-                    "type": "string",
-                    "default": ""
-                },
-                "name": {
-                    "description": "Name",
-                    "type": "string",
-                    "default": ""
-                }
+module.exports = codebox.settings.schema("git",
+    {
+        "title": "Git",
+        "type": "object",
+        "properties": {
+            "email": {
+                "description": "Email",
+                "type": "string",
+                "default": ""
+            },
+            "name": {
+                "description": "Name",
+                "type": "string",
+                "default": ""
             }
         }
-    );
-});
+    }
+);
